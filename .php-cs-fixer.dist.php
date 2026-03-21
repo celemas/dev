a2ca08e6c4619bb8b1b1f06fa4225a618186bf56
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2026-03-21 11:13:49 +0100

    Update phpunit/coverlyzer, remove php-cs-fixer

--- .php-cs-fixer.dist.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-$finder = PhpCsFixer\Finder::create()->in(__DIR__ . '/src');
-$config = new Duon\Development\PhpCsFixer\Config();
-
-return $config->setFinder($finder);
