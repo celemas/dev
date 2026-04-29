74ad5e1e6d96488c419d6197184b5f822a538ea4
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2026-04-29 18:08:29 +0200

    Fix docs runner argument check

--- src/DocsTestRunner.php
+++ src/DocsTestRunner.php
@@ -13,17 +13,19 @@ class DocsTestRunner
 	{
 		$this->title = 'Documentation snippets test runner';
 
-		if (!isset($args[1])) {
+		$dir = $args[1] ?? null;
+
+		if ($dir === null) {
 			print "{$this->title}: Missing directory operand" . PHP_EOL;
 			print 'Try `docs-test-runner path/to/snippets`' . PHP_EOL;
 			exit(1);
 		}
 
-		$this->dir = realpath($args[1]);
+		$this->dir = realpath($dir);
 
 		if (!$this->dir || !is_dir($this->dir)) {
 			print "{$this->title}: Directory does not exist" . PHP_EOL;
-			print 'Directory: ' . $args[1] . PHP_EOL;
+			print 'Directory: ' . $dir . PHP_EOL;
 			exit(1);
 		}
 	}
